ccec2463b76853e8da9a4e03452fdc25f0feb7e8
flo313 <31136223+flo313@users.noreply.github.com>
2017-12-01 20:21:09 +0100

    Add files via upload

---
+++ start_tvh.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function ts {
+  echo "[`date '+%Y-%m-%d %T'`] `basename "$(test -L "$0" && readlink "$0" || echo "$0")"`:"
+}
+
+echo "$(ts) Creating group and user "
+# Create User and make Premissions on Folders
+addgroup -g ${USER_ID} ${USER_NAME}
+adduser  -H -D -S -u ${USER_ID} -G ${USER_NAME} ${USER_NAME}
+
+chown -R ${USER_NAME}:${USER_NAME} $CONFIG_DIR $RECORD_DIR
+
+echo "$(ts) Starting Tvheadend"
+/usr/bin/tvheadend --firstrun -u $USER_NAME -g $USER_NAME -c /config --http_root /tvheadend/
