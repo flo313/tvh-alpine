b5336e1950eab13ed2fb50d69956b4e059710fd2
flo313 <31136223+flo313@users.noreply.github.com>
2018-01-22 12:06:38 +0100

    Update start_tvh.sh

--- start_tvh.sh
+++ start_tvh.sh
@@ -10,6 +10,7 @@ addgroup -g ${USER_ID} ${USER_NAME}
 adduser  -H -D -S -u ${USER_ID} -G ${USER_NAME} ${USER_NAME}
 
 chown -R ${USER_NAME}:${USER_NAME} $CONFIG_DIR $RECORD_DIR
+chmod -R 755 $CONFIG_DIR $RECORD_DIR
 
 echo "$(ts) Starting Tvheadend"
 /usr/bin/tvheadend --firstrun -u $USER_NAME -g $USER_NAME -c /config --http_root /tvheadend/
