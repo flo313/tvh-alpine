7bb6ef38374b9022f7a2e0f20950be20597195c7
flo313 <31136223+flo313@users.noreply.github.com>
2018-03-22 17:43:29 +0000

    Update start_tvh.sh

--- start_tvh.sh
+++ start_tvh.sh
@@ -4,13 +4,15 @@ function ts {
   echo "[`date '+%Y-%m-%d %T'`] `basename "$(test -L "$0" && readlink "$0" || echo "$0")"`:"
 }
 
-echo "$(ts) Creating group and user "
+echo "$(ts) Creating group and user ${USER_NAME} (id ${USER_ID})..."
 # Create User and make Premissions on Folders
 addgroup -g ${USER_ID} ${USER_NAME}
 adduser  -H -D -S -u ${USER_ID} -G ${USER_NAME} ${USER_NAME}
 
+echo "$(ts) Set user ${USER_NAME} (id ${USER_ID}) owner of $CONFIG_DIR and $RECORD_DIR..."
 chown -R ${USER_NAME}:${USER_NAME} $CONFIG_DIR $RECORD_DIR
+echo "$(ts) Set permissions of $CONFIG_DIR and $RECORD_DIR to 755..."
 chmod -R 755 $CONFIG_DIR $RECORD_DIR
 
-echo "$(ts) Starting Tvheadend"
+echo "$(ts) Starting Tvheadend with $USER_NAME"
 /usr/bin/tvheadend --firstrun -u $USER_NAME -g $USER_NAME -c /config --http_root /tvheadend/
