a4e28f500dcd93c9c0666d869550a930628491c8
flo313 <31136223+flo313@users.noreply.github.com>
2018-03-27 22:53:24 +0200

    Update start_tvh.sh

--- start_tvh.sh
+++ start_tvh.sh
@@ -12,7 +12,7 @@ adduser  -H -D -S -u ${USER_ID} -G ${USER_NAME} ${USER_NAME}
 echo "$(ts) Set user ${USER_NAME} (id ${USER_ID}) owner of $CONFIG_DIR and $RECORD_DIR..."
 chown -R ${USER_NAME}:${USER_NAME} $CONFIG_DIR $RECORD_DIR
 echo "$(ts) Set permissions of $CONFIG_DIR and $RECORD_DIR to 755..."
-chmod -R 755 $CONFIG_DIR $RECORD_DIR
+chmod -R u+rwx $CONFIG_DIR $RECORD_DIR
 
 echo "$(ts) Starting Tvheadend with $USER_NAME"
 /usr/bin/tvheadend --firstrun -u $USER_NAME -g $USER_NAME -c /config --http_root /tvheadend/
